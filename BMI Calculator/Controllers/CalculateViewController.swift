56bb93a38923cf915834c3619d5501570123963c
Ozlem Cali <ozlemmcali@gmail.com>
2022-12-14 11:20:13 +0300

    first commit

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    var bmiValue = "0.0"
+    
+    var calculateBrain = CalculatorBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+        
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        calculateBrain.calculateBMI(height:height, weight : weight)
+        performSegue(withIdentifier: "goToResult", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult"{
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculateBrain.getBMIValue()
+            destinationVC.advice = calculateBrain.getAdvice()
+            destinationVC.color = calculateBrain.getColor()
+        }
+    }
+}
+
---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by ozlem on 30.11.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    var bmiValue : String?
+    var advice : String?
+    var color : UIColor?
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+   
+  
+
+}
